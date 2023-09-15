de286487fdd006b9f282add1e05d4db7fa300a4b
Yasmin Braz A Silva <141586083+YasminBrazASilva@users.noreply.github.com>
2023-09-15 08:34:53 -0300

    Add files via upload
    Atualização com o campo "empresa" e refatoração do código para otimização.

--- src/js/index.js
+++ src/js/index.js
@@ -9,56 +9,65 @@ const grupoProgresso = document.getElementById('grupoProgresso');
 const barraProgresso = document.getElementById('barraProgresso');
 const progressoDetalhe = document.getElementById('progressoDetalhe');
 const textoFinal = document.getElementById('textoFinal');
-const textosResultados = document.getElementById('textosResultados');
+const imagensResultados = document.getElementById('imagensResultados');
 
 let dados = {};
 let indiceQuestaoAtual = 0;
-let nomeCompletoResposta = '';
-let emailResposta = '';
-const respostasSelecionadas = [];
+let respostasSelecionadas = [];
 
-const questoes = [
-    { opcoes: ['Quantitativo', 'Estruturado', 'Adaptável', 'Tradicional'] },
-    { opcoes: ['Variedade', 'Relacionamento', 'Estabilidade', 'Análise'] },
-    { opcoes: ['Método', 'Perspectiva', 'Experimento', 'Aleatório'] },
-    { opcoes: ['Diplomático', 'Reservado', 'Entusiasmado', 'Analítico'] },
-    { opcoes: ['Visionário', 'Organizado', 'Colaborativo', 'Curioso'] },
-    { opcoes: ['Integridade', 'Consistência', 'Possibilidades', 'Consenso'] },
-    { opcoes: ['Equilíbrio', 'Foco', 'Conexão', 'Imaginação'] },
-    { opcoes: ['Desafios', 'Previsão', 'Motivação', 'Execução'] },
-    { opcoes: ['Renovação', 'Prevenção', 'Regras/Normas', 'Comando/Direção'] },
-    { opcoes: ['Inovação', 'Parceria', 'Ordem', 'Imaginação'] },
-    { opcoes: ['Plano detalhado', 'Esforço direcionado', 'Conflito administrativo', 'Panorama geral'] },
-    { opcoes: ['Competência', 'Maturidade', 'Amizade', 'Criatividade'] },
-    { opcoes: ['Quanto?', 'Por que?', 'Quem?', 'Quando?'] },
-    { opcoes: ['Empatia', 'Lealdade', 'Lógica', 'Espontaneidade'] },
-    { opcoes: ['Vencer', 'Manter', 'Recuar', 'Arriscar'] }
-];
+const questoes = { 
+    1: ['Quantitativo', 'Estruturado', 'Adaptável', 'Tradicional'],
+    2: ['Variedade', 'Relacionamento', 'Estabilidade', 'Análise'],
+    3: ['Método', 'Perspectiva', 'Experimento', 'Aleatório'],
+    4: ['Diplomático', 'Reservado', 'Entusiasmado', 'Analítico'],
+    5: ['Visionário', 'Organizado', 'Colaborativo', 'Curioso'],
+    6: ['Integridade', 'Consistência', 'Possibilidades', 'Consenso'],
+    7: ['Equilíbrio', 'Foco', 'Conexão', 'Imaginação'],
+    8: ['Desafios', 'Previsão', 'Motivação', 'Execução'],
+    9: ['Renovação', 'Prevenção', 'Regras/Normas', 'Comando/Direção'],
+    10: ['Inovação', 'Parceria', 'Ordem', 'Imaginação'],
+    11: ['Plano detalhado', 'Esforço direcionado', 'Conflito administrativo', 'Panorama geral'],
+    12: ['Competência', 'Maturidade', 'Amizade', 'Criatividade'],
+    13: ['Quanto?', 'Por que?', 'Quem?', 'Quando?'],
+    14: ['Empatia', 'Lealdade', 'Lógica', 'Espontaneidade'],
+    15: ['Vencer', 'Manter', 'Recuar', 'Arriscar'] 
+};
 
+const qtdeQuestoes = Object.keys(questoes).length;
 
 botaoComecar.addEventListener('click', () => {
     comecar();
 });
 
 function comecar() {
-    botaoComecar.classList.add('oculto');
-    grupoIntroducao.classList.add('oculto');
+    ocultarElemento(botaoComecar);
+    ocultarElemento(grupoIntroducao);
 
-    grupoDetalheUsuario.classList.remove('oculto');
-}
+    exibirElemento(grupoDetalheUsuario);
+};
+
+function ocultarElemento(elemento) {
+    elemento.classList.add('oculto');
+};
+
+function exibirElemento(elemento) {
+    elemento.classList.remove('oculto');
+};
 
 
 botaoOk.addEventListener('click', () => {
-    nomeCompletoResposta = document.getElementById('nomeCompleto').value;
-    emailResposta = document.getElementById('email').value;
+    const nomeCompletoResposta = document.getElementById('nomeCompleto').value;
+    const emailResposta = document.getElementById('email').value;
+    const empresaResposta = document.getElementById('empresa').value;
 
-    if (!nomeCompletoResposta || !emailResposta) {
-        alert('Por favor, preencha seu nome completo e e-mail.');
+    if (!nomeCompletoResposta || !emailResposta || !empresaResposta) {
+        alert('Por favor, preencha todas as informações.');
         return;
     }
 
     salvarDados("nomeCompleto", nomeCompletoResposta);
     salvarDados("email", emailResposta);
+    salvarDados("empresa", empresaResposta)
     iniciarTeste();
 });
 
@@ -68,87 +77,111 @@ function salvarDados(titulo, objeto) {
 };
 
 function iniciarTeste() {
-    grupoDetalheUsuario.classList.add('oculto');
+    ocultarElemento(grupoDetalheUsuario);
 
-    grupoProgresso.classList.remove('oculto');
-    grupoOpcoes.classList.remove('oculto');
+    exibirElemento(grupoProgresso);
+    exibirElemento(grupoOpcoes);
     mostrarQuestao(0);
-}
+};
 
 function mostrarQuestao(indiceQuestaoAtual) {
-    const questao = questoes[indiceQuestaoAtual];
-    grupoOpcoes.innerHTML = '';
+    const alternativas = questoes[indiceQuestaoAtual+1];
+    atualizarTextoHTML(grupoOpcoes, '');
 
-    const gridOpcoes = document.createElement('div');
-    gridOpcoes.classList.add('gridOpcoes');
-
-    questao.opcoes.forEach((opcao, opcaoIndex) => {
-        const alternativa = document.createElement('button');
-
+    alternativas.forEach((opcao, opcaoIndex) => {
+        const alternativa = criarElementoHTML('button');
         alternativa.textContent = opcao;
+        
+        adicionarElementoHTMLFilho(grupoOpcoes, alternativa);
+
         alternativa.addEventListener('click', () => {
-
-            removerSelecaoAnterior();
-            alternativa.classList.add('selecionada');
+            adicionarSelecao(alternativa);            
             armazenarResposta(opcaoIndex);
-
         });
-        gridOpcoes.appendChild(alternativa);
-
     });
-    grupoOpcoes.appendChild(gridOpcoes);
-}
+};
+
+function atualizarTextoHTML(elemento, texto) {
+    elemento.innerHTML = texto;
+};
+
+function criarElementoHTML(tagName, className) {
+    const elemento = document.createElement(tagName);
+    if (className) {
+        elemento.classList.add(className);
+    }
+    return elemento;
+};
+
+function adicionarElementoHTMLFilho(pai, filho) {
+    pai.appendChild(filho);
+};
+
+function adicionarSelecao(alternativa) {
+    removerSelecaoAnterior();
+    alternativa.classList.add('selecionada');
+};
 
 function removerSelecaoAnterior() {
     const alternativaJaSelecionada = document.querySelector('.selecionada');
+
     if (alternativaJaSelecionada) {
         alternativaJaSelecionada.classList.remove('selecionada');
-    }
-}
+    };
+};
 
-function armazenarResposta(selectedIndex) {
-    respostasSelecionadas[indiceQuestaoAtual] = selectedIndex;
+function armazenarResposta(indiceSelecionado) {
+    respostasSelecionadas[indiceQuestaoAtual] = indiceSelecionado;
+    chamarBotaoAvancar();
+};
 
-    if (indiceQuestaoAtual < questoes.length - 1) {
-        botaoProxima.classList.remove('oculto');
+function chamarBotaoAvancar() {
+    const indiceUltimaPergunta = qtdeQuestoes - 1;
+
+    if (indiceQuestaoAtual < indiceUltimaPergunta) {
+        exibirElemento(botaoProxima);
     } else {
-        botaoEnviar.classList.remove('oculto');
+        exibirElemento(botaoEnviar);
     }
 };
 
 
 botaoProxima.addEventListener('click', () => {
-    indiceQuestaoAtual++;
-    botaoProxima.classList.add("oculto");
-
-    if (indiceQuestaoAtual < questoes.length) {
-        mostrarQuestao(indiceQuestaoAtual);
-        atualizarProgresso();
-    }
+    avancarQuestao();
 });
 
+function avancarQuestao() {
+    indiceQuestaoAtual++;
+    ocultarElemento(botaoProxima);
+
+    if (indiceQuestaoAtual < qtdeQuestoes) {
+        mostrarQuestao(indiceQuestaoAtual);
+        atualizarProgresso();
+    };
+};
+
 function atualizarProgresso() {
-    const progressoAtual = (indiceQuestaoAtual / questoes.length) * 100;
+    const progressoAtual = (indiceQuestaoAtual / qtdeQuestoes) * 100;
     progressoDetalhe.textContent = `${progressoAtual.toFixed(1)}%`;
     barraProgresso.value = progressoAtual;
 };
 
 
 botaoEnviar.addEventListener('click', () => {
-    botaoEnviar.classList.add("oculto");
     finalizarTeste();
 });
 
+
 function finalizarTeste() {
-    grupoOpcoes.classList.add("oculto");
-    grupoProgresso.classList.add("oculto");
+    ocultarElemento(botaoEnviar);
+    ocultarElemento(grupoOpcoes);
+    ocultarElemento(grupoProgresso);
 
     const [resultadoFinal, descricaoFinal] = imprimirOResultado();
 
-    textoFinal.innerHTML = resultadoFinal;
-    textosResultados.innerHTML = descricaoFinal;
-    criarBarrasDePorcentagem()
-
+    atualizarTextoHTML(textoFinal, resultadoFinal);
+    atualizarTextoHTML(imagensResultados, descricaoFinal);
+    criarBarrasDePorcentagem();
 
     enviarDadosParaPlanilha(dados);
 
@@ -160,18 +193,17 @@ function criarBarrasDePorcentagem() {
 
     for (const perfil in perfisPorcentagens) {
         const porcentagem = perfisPorcentagens[perfil];
-        const barra = document.createElement('div');
+
+        const barra = criarElementoHTML('div', 'barra-porcentagem');
         barra.style.backgroundColor = coresPerfis[perfil];
         barra.style.width = `${porcentagem}%`;
-        barra.classList.add('barra-porcentagem'); // Classe para estilizar as barras no CSS
+        adicionarElementoHTMLFilho(barraPorcentagemContainer, barra);
 
-        const label = document.createElement('span');
-        label.textContent = `${porcentagem}%`;
-
-        barra.appendChild(label);
-        barraPorcentagemContainer.appendChild(barra);
-    }
-}
+        const detalhe = criarElementoHTML('span');
+        detalhe.textContent = `${porcentagem}%`;
+        adicionarElementoHTMLFilho(barra, detalhe);
+    };
+};
 
 
 function enviarDadosParaPlanilha(dados) {
@@ -190,6 +222,7 @@ function enviarDadosParaPlanilha(dados) {
             console.error('Erro ao armazenar os dados na planilha:', error);
         });
 };
+
 
 
 // CÁLCULOS PARA DEFINIR O PERFIL DO USUÁRIO
@@ -212,44 +245,38 @@ const gabarito = [
 ];
 
 const perfis = {
-    'Condutor': 'D',
-    'Guardião': 'G',
-    'Conciliador': 'I',
-    'Pioneiro': 'P'
+    'D': ['Condutor', 'condutor'],
+    'G': ['Guardião', 'guardiao'],
+    'I': ['Conciliador', 'conciliador'],
+    'P': ['Pioneiro', 'pioneiro']
 };
 
 
 const descricaoPerfis = {
-    'Condutor': `<img src="./src/imagens/Condutores.jpg" alt="Imagem de Condutores" class="perfil-imagem">`,
-    'Guardião': `<img src="./src/imagens/Guardioes.jpg" alt="Imagem de Guardioes" class="perfil-imagem">`,
-    'Conciliador': `<img src="./src/imagens/Conciliadores.jpg" alt="Imagem de Conciliadores" class="perfil-imagem">`,
-    'Pioneiro': `<img src="./src/imagens/Pioneiros.jpg" alt="Imagem de Pioneiros" class="perfil-imagem">`
+    'D': `<img src="./src/imagens/Condutores.jpg" alt="Imagem de Condutores" class="perfil-imagem">`,
+    'G': `<img src="./src/imagens/Guardioes.jpg" alt="Imagem de Guardioes" class="perfil-imagem">`,
+    'I': `<img src="./src/imagens/Conciliadores.jpg" alt="Imagem de Conciliadores" class="perfil-imagem">`,
+    'P': `<img src="./src/imagens/Pioneiros.jpg" alt="Imagem de Pioneiros" class="perfil-imagem">`
 };
 
 const coresPerfis = {
-    'Condutor': '#F03E24',
-    'Guardião': '#4A70B7',
-    'Conciliador': '#43A848',
-    'Pioneiro': '#FDAF1B'
+    'D': '#F03E24',
+    'G': '#4A70B7',
+    'I': '#43A848',
+    'P': '#FDAF1B'
 };
 
 
 function imprimirOResultado() {
-    let perfisResposta = definirOPerfil();
+    const perfisResposta = definirOPerfil();
+    
+    const resultadoFinal = `<div>TESTE FINALIZADO!</div>
+    <div>Parabéns, o seu perfil é: </div>
+    <div>${perfisResposta.map(perfil => `<span style="color: ${coresPerfis[perfil]};">${perfis[perfil][0].toUpperCase()}</span>`).join(' ')}</div>`;
 
-    let resultadoFinal = `<div>TESTE FINALIZADO!</div>
-    <div>Parabéns, o seu perfil é:</div>`;
-    let descricaoFinal = '';
+    const descricaoFinal = perfisResposta.map(perfil => `<br><div>${descricaoPerfis[perfil]}</div>`).join('');
 
-    for (const perfil of perfisResposta) {
-        const corPerfil = coresPerfis[perfil];
-        const spanTag = `<span style="color: ${corPerfil};">${perfil.toUpperCase()}</span>`
-        resultadoFinal += ` ${spanTag}`;
-        descricaoFinal += `<br><div>${descricaoPerfis[perfil]}</div>`;
-    };
-
-    const perfilFinal = perfisResposta.map(item => item.toUpperCase()).join(' ');
-    salvarDados("perfil", perfilFinal);
+    salvarDados("perfil", perfisResposta.map(perfil => perfis[perfil][0].toUpperCase()).join(' '));
 
     return [resultadoFinal, descricaoFinal];
 };
@@ -261,42 +288,32 @@ function definirOPerfil() {
         Object.entries(porcentagens).sort((a, b) => b[1] - a[1])
     );
 
-    const perfisOrdenados = Object.keys(porcentagensOrdenadas);
-    const valoresOrdenados = Object.values(porcentagensOrdenadas);
+    const perfisResposta = Object.keys(porcentagensOrdenadas)
+        .filter((perfil) => porcentagensOrdenadas[perfil] > 25)
 
-    const [valorTop1, valorTop2, valorTop3] = valoresOrdenados.slice(0, 3);
-
-    let perfisResposta = [];
-
-    if (valorTop3 > 25) {
-        perfisResposta = perfisOrdenados.slice(0, 3);
-    } else if (valorTop2 > 25) {
-        perfisResposta = perfisOrdenados.slice(0, 2);
-    } else {
-        perfisResposta = perfisOrdenados.slice(0, 1);
-    };
     return perfisResposta;
-}
+};
 
 function calcularAsPorcentagens() {
     const letrasEcolhidas = checarGabarito();
-    const qtdeAlternativas = gabarito.length;
+    let repeticoesPorPerfil = {};
 
-    const qtdeLetras = {};
-    for (let i = 0; i < letrasEcolhidas.length; i++) {
-        const letra = letrasEcolhidas[i];
-        qtdeLetras[letra] = (qtdeLetras[letra] || 0) + 1;
-    }
+    for (const perfil in perfis) {
+        repeticoesPorPerfil[perfil] = 0;
+    };
+
+    letrasEcolhidas.forEach(letraEscolhida => {
+        repeticoesPorPerfil[letraEscolhida]++;
+    });
 
     const resultado = {};
-    const perfisChaves = Object.keys(perfis);
+    for (const letraPerfil in perfis) {
+        const qtdeRepeticoes = repeticoesPorPerfil[letraPerfil];
+        resultado[letraPerfil] = parseFloat((qtdeRepeticoes / qtdeQuestoes * 100).toFixed(1));
 
-    for (const perfil of perfisChaves) {
-        const qtde = qtdeLetras[perfis[perfil]];
-        const qtdeNumerico = isNaN(qtde) ? 0 : qtde;
-        resultado[perfil] = parseFloat((qtdeNumerico / qtdeAlternativas * 100).toFixed(1));
-        salvarDados(perfil, `${resultado[perfil]}%`)
-    }
+        const nomesPerfil = perfis[letraPerfil][1];
+        salvarDados(nomesPerfil, `${resultado[letraPerfil]}%`)
+    };
 
     return resultado;
 };
@@ -305,12 +322,13 @@ function checarGabarito() {
     const respostasLetras = [];
 
     for (let i = 0; i < respostasSelecionadas.length; i++) {
-        const letra = gabarito[i][respostasSelecionadas[i]];
-        respostasLetras.push(letra);
-    };
+        const respostaSelecionada = [respostasSelecionadas[i]];
 
-    for (let i = 0; i < respostasLetras.length; i++) {
-        salvarDados(`pergunta${i+1}`, respostasLetras[i]);
+        const gabaritoPergunta = gabarito[i];
+        const letraSelecionada = gabaritoPergunta[respostaSelecionada];
+        
+        salvarDados(`pergunta${i+1}`, letraSelecionada);
+        respostasLetras.push(letraSelecionada);
     };
 
     return respostasLetras;
